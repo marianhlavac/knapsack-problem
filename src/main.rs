c59d5f05401b046afc2c4d4998b7b227d4fb6dee
Marián Hlaváč <hlavac95@gmail.com>
2017-10-15 22:31:22 +0200

    Fix overflowing bugs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+extern crate time;
+
 mod parser;
 mod solver; 
 use parser::Knapsack;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
+use std::io::prelude::*;                                                           
+use std::io; 
 
 fn read_file(file_path: &str) -> BufReader<File> {
     return BufReader::new(match File::open(file_path) {
@@ -12,20 +16,22 @@ fn read_file(file_path: &str) -> BufReader<File> {
 }
 
 fn main() {
-    let file = read_file("data/knap_15.inst.dat");
+    let file = read_file("data/knap_20.inst.dat");
     
     let knapsacks: Vec<Knapsack> = file.lines().map(|line| {
         parser::parse_knapsack(&line.unwrap())
     }).collect();
     
-    for knapsack in knapsacks {
-        println!("--- {:?}", knapsack);
-        
-        let solutionsf = solver::solve_first(&knapsack);
-        println!("Solve First Method: {:?} {}", solutionsf, if solver::validate(&solutionsf, &knapsack) {"is valid"} else {"is not valid"});
-        
-        let solutionbf = solver::solve_bruteforce(&knapsack);
-        println!("Bruteforce Method: {:?} {}", solutionbf, if solver::validate(&solutionbf, &knapsack) {"is valid"} else {"is not valid"});
+    for knapsack in knapsacks { 
+        print!("METHOD_BRUTEFORCE\t\tKSID: {}\t\t", knapsack.id);   
+        io::stdout().flush().ok(); 
+        let sol = solver::solve_bruteforce(&knapsack);
+        println!("P: {}\tW: {}\t\t{}VALID\tEL: {}ms", 
+            sol.price,
+            sol.weight, 
+            if solver::validate(&sol, &knapsack) {""} else {"IN"},
+            sol.elapsed,
+        );
     }
     
     
--- src/solver.rs
+++ src/solver.rs
@@ -5,62 +5,51 @@ use std::u32;
 
 #[derive(Debug, Clone, Copy)]
 pub struct KnapSolution {
-    knap_id: u16,
-    pub bitmask: u16,
+    pub knap_id: u16,
+    pub bitmask: u32,
     pub price: u16,
     pub weight: u16,
     pub elapsed: u16,
 }
 
-    
-    
-    
-}
-
-fn items_from_bmask(knap: &Knapsack, bitmask: u16) -> Vec<&KnapItem> {
+fn items_from_bmask(knap: &Knapsack, bitmask: u32) -> Vec<&KnapItem> {
     knap.items.iter().enumerate().filter(|item| {
         bitmask & (1 << item.0) != 0
     }).map(|item| item.1).collect()
 }
 
+fn bitmask_from_items(items: Vec<&KnapItem>) -> u16 {
+    return 0
+}
+
 fn calc_fitness(items: Vec<&KnapItem>) -> (u16, u16) {
     items.iter().fold((0, 0), |acc, ref item| (acc.0 + item.weight, acc.1 + item.price))
+    // let mut fit : (u16, u16) = (0, 0);
+    // for item in items {
+    //     fit.0 += item.weight;
+    //     fit.1 += item.price;
+    // }
+    // 
+    // fit
 }
 
 pub fn validate(solution: &KnapSolution, knap: &Knapsack) -> bool {
-    solution.weight < knap.capacity
-}
-
-pub fn solve_first(knap: &Knapsack) -> KnapSolution {
-    let mut bitmask = u16::pow(2, knap.items.len() as u32) - 1;
-    let mut fitness = calc_fitness(items_from_bmask(knap, bitmask));
-        
-    while fitness.0 > knap.capacity {
-        bitmask = bitmask >> 1;
-        fitness = calc_fitness(items_from_bmask(knap, bitmask));
-    }
-    
-    KnapSolution {
-        knap_id: knap.id,
-        bitmask: bitmask,
-        weight: fitness.0,
-        price: fitness.1,
-    }
+    solution.weight <= knap.capacity
 }
 
 pub fn solve_bruteforce(knap: &Knapsack) -> KnapSolution {
     let start = PreciseTime::now();
     let mut bitmask = 0;
     let mut fitness = calc_fitness(items_from_bmask(knap, bitmask));
-    let mut best = (0, 0);
+    let mut best = (fitness.1, 0);
     
-    let max_bitmask = u16::pow(2, knap.items.len() as u32);
+    let max_bitmask = u32::pow(2, knap.items.len() as u32);
         
     while bitmask < max_bitmask {
         bitmask += 1;
         fitness = calc_fitness(items_from_bmask(knap, bitmask));
         
-        if (fitness.0 <= knap.capacity && fitness.1 > best.0) {
+        if fitness.0 <= knap.capacity && fitness.1 > best.0 {
             best = (fitness.1, bitmask);
         }
     }
@@ -78,16 +67,12 @@ pub fn solve_bruteforce(knap: &Knapsack) -> KnapSolution {
     }
 }
 
-// pub fn solve_random(knap: Knapsack) -> KnapSolution {
-//     
-// }
-// 
-
-// 
-// pub fn solve_greedy(knap: Knapsack) -> KnapSolution {
-//     
-// }
-// 
-// pub fn solve_heuristic(knap: Knapsack) -> KnapSolution {
-//     
-// }
+pub fn solve_heuristic(knap: Knapsack) -> KnapSolution {
+    KnapSolution {
+        knap_id: 0,
+        bitmask: 0,
+        weight: 0,
+        price: 0,
+        elapsed: 0,
+    }
+}

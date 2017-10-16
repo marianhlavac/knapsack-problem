8dcd7bc6643d71a6af8226e795966ff9b4359c98
Marián Hlaváč <hlavac95@gmail.com>
2017-10-16 23:21:03 +0200

    Add CSV reporter

--- src/reporter.rs
+++ src/reporter.rs
@@ -1,15 +1,31 @@
+use parser::{Knapsack, KnapItem};
 use solver::KnapSolution;
 
-pub fn report_display(solution: &KnapSolution) {
-    println!("TYPE: {:?}\tKNAP_ID: {}\tPRICE: {}\tWEIGHT: {}\tELAPSED: {}ms", 
+pub fn report_display(knap: &Knapsack, solution: &KnapSolution) {
+    println!("KNAP_ID: {}\tSIZE: {}\t{:?}\tPRICE: {}\tWEIGHT: {}/{}\tELAPSED: {}ms", 
+        knap.id,
+        knap.items.len(),
         solution.soltype,
-        solution.knap_id,
         solution.price,
         solution.weight,
+        knap.capacity,
         solution.elapsed,
     );
 }
 
-pub fn report_csv(solution: &KnapSolution) {
-    println!("not implemented");
+pub fn report_csv_head() {
+    println!("knap_id;item_count;capacity;sol_type;price;weight;bitmask;elapsed");
+}
+
+pub fn report_csv(knap: &Knapsack, solution: &KnapSolution) {
+    println!("{};{};{};{:?};{};{};{};{}", 
+        knap.id,
+        knap.items.len(),
+        knap.capacity,
+        solution.soltype,
+        solution.price,
+        solution.weight,
+        solution.bitmask,
+        solution.elapsed,
+    );
 }

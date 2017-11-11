20d3464f8aeec28848b8652e09803a9d28327842
Marián Hlaváč <hlavac95@gmail.com>
2017-11-11 22:49:06 +0100

    Generate some more

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 echo "Retrieve solutions..."
 ./solutions.sh data/knap*sol.dat > solutions.csv
 echo "Generating, give it a while..."
-cargo run --release data/knap_{4,10,15,20,22,25,27,30}.inst.dat > results.csv
+cargo run --release data/knap_{4,10,15,20,22,25,27,30,32}.inst.dat > results.csv

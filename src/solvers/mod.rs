22b941df76d4a1a1eb50b6ba5576a78f7ad5cfb0
Marián Hlaváč <hlavac95@gmail.com>
2017-12-15 13:22:52 +0100

    Rename solvers to better reflect new dir struct

--- src/solvers/mod.rs
+++ src/solvers/mod.rs
@@ -1,9 +1,10 @@
-use parser::{Knapsack, SolutionType};
+use parser::{bit_substructures, Knapsack, SolutionType};
 use solver_recursive;
 use solver_branchandbound;
 use solver_heuristic;
 use solver_dynamic;
 use solver_fptas;
+use genetic;
 use time::PreciseTime;
 
 /// Solves an instance of knapsack problem, returning solved knapsack.
@@ -19,6 +20,7 @@ pub fn solve(knap: &Knapsack, soltype: SolutionType) -> Knapsack {
         SolutionType::FPTAS25 => solver_fptas::solve(knap.clone(), 0.25),
         SolutionType::FPTAS50 => solver_fptas::solve(knap.clone(), 0.5),
         SolutionType::FPTAS75 => solver_fptas::solve(knap.clone(), 0.75),
+        SolutionType::Evolution => solve_by_evolution(knap.clone(), 8),
         SolutionType::None => knap.clone(),
     };
     
@@ -26,4 +28,23 @@ pub fn solve(knap: &Knapsack, soltype: SolutionType) -> Knapsack {
     solved.elapsed = if elapsed_t.num_milliseconds() <= 1 { elapsed_t.num_nanoseconds().unwrap() as f32 / 1000000.0 } else { elapsed_t.num_milliseconds() as f32 };
     
     solved
+}
+
+/// Starts an evolution algorithm for solving the problem.
+fn solve_by_evolution(knap: Knapsack, pop_size: usize) -> Knapsack {
+    // Get values substructures
+    let (prices, weights) = parser::bit_substructures(&knap);
+    
+    // Define a fitness function
+    let fitness_fn = |x: BitVec| {
+        prices.iter().enumerate().fold(0, |sum, (i, price)| if (x.get(i)) { sum += price });
+    }
+    
+    // Define a constraint function
+    let constraint_fn = |x: BitVec| {
+        let weight = weights.iter().enumerate().fold(0, |sum, (i, w)| if (x.get(i)) { sum += w });
+        weight <= knap.capacity;
+    }
+    
+    genetic::simulate(pop_size, fitness_fn, constraint_fn);
 }
